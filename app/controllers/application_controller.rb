6f499e9816b31f14799fa28ac031350725b49de0
Yoshihide Chubachi <yoshi@chubachi.net>
2019-11-04 21:06:27 +0000

    Update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
-  rescue_from Exception, with: :render_500
+  # rescue_from Exception, with: :render_500
 
-  def render_500(e)
-    ExceptionNotifier.notify_exception(e, :env => request.env, :data => {:message => "error"})
-    render template: 'errors/error_500', status: 500
-  end
+  # def render_500(e)
+  #   ExceptionNotifier.notify_exception(e, :env => request.env, :data => {:message => "error"})
+  #   render template: 'errors/error_500', status: 500
+  # end
 end
