6765264857d38dcb2b350e1f233a5f15897b9230
Yoshihide Chubachi <yoshi@chubachi.net>
2019-11-04 21:42:48 +0000

    Update

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -45,7 +45,7 @@ ExceptionNotification.configure do |config|
   # }
 
   config.add_notifier :slack, {
-    :webhook_url => ENV['SLACK_WEBHOOK_URL']
+    :webhook_url => ENV['SLACK_WEBHOOK_URL'],
     :channel => "#tradingview"
   }
 end
