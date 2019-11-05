9725c08c44144193f49d4090f1ce227881a7b540
Yoshihide Chubachi <yoshi@chubachi.net>
2019-11-05 12:12:09 +0000

    Update

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module TradingviewWebhook
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.paths.add 'lib', eager_load: true
   end
 end
