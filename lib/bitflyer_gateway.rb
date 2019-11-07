904320d4eb23ab65e42b43722833ba2f89a64724
Yoshihide Chubachi <yoshi@chubachi.net>
2019-11-07 15:24:38 +0000

    Refactoring

--- lib/bitflyer_gateway.rb
+++ lib/bitflyer_gateway.rb
@@ -1,13 +1,31 @@
 require 'bitflyer'
 
+module Retriable
+  def with_retry(max_attempts: 5)
+    # puts "#{__method__}: ENTER"
+    attempts = 0
+    begin
+      attempts += 1
+      yield
+    rescue RetryError => e
+      puts "#{__method__}: retry #{attempts}/#{max_attempts}"
+      raise e.cause if max_attempts < attempts
+      retry
+    end
+    # puts "#{__method__}: EXIT"
+  end
+end
+
 class BitflyerGateway
+  include Retriable
+
   def initialize(key, secret)
-    puts "#{__method__}: ENTER"
+    # puts "#{__method__}: ENTER"
     @key = key
     @secret = secret
     @public_client  = Bitflyer.http_public_client
     @private_client = Bitflyer.http_private_client(key, secret)
-    puts "#{__method__}: EXIT"
+    # puts "#{__method__}: EXIT"
   end
 
   def long(size: nil, profit: 1000.0, loss: 500.0, risk: 0.02)
@@ -41,44 +59,31 @@ class BitflyerGateway
   end
 
   def close_all
-    puts "#{__method__}: ENTER"
-    puts "#{__method__}: CALL: cancel_all_child_orders"
-    r = @private_client.cancel_all_child_orders(product_code: 'FX_BTC_JPY')
-    puts "#{__method__}: r = #{r}"
+    puts "#{__method__}: ENTER()"
+    # ==========================================================================
+    # 今ある全注文を取り消す
+    # --------------------------------------------------------------------------
+    cancel_all_child_orders
+    # --------------------------------------------------------------------------
 
+    # ==========================================================================
+    # 今ある建玉のサイズを取得する
+    # --------------------------------------------------------------------------
     sizes = position_sizes
     puts "#{__method__}: sizes = #{sizes}"
+    # --------------------------------------------------------------------------
 
-    try = 0
-    begin
-      try += 1
-      if sizes[:buy] > 0
-        puts "#{__method__}: CALL: send_child_order"
-        r =  @private_client.send_child_order(product_code: 'FX_BTC_JPY',
-          child_order_type: 'MARKET', side: 'SELL', size: sizes[:buy])
-        if r['status'] != nil
-          raise r.to_s # 発注失敗
-        end
-        puts "#{__method__}: r = #{r}"
-      elsif sizes[:sell] > 0
-        puts "#{__method__}: CALL: send_child_order"
-        r = @private_client.send_child_order(product_code: 'FX_BTC_JPY',
-          child_order_type: 'MARKET', side: 'BUY',  size: sizes[:sell])
-        if r['status'] != nil
-          raise r.to_s # 発注失敗
-        end
-        puts "#{__method__}: r = #{r}"
-      end
-    rescue
-      sleep(0.1)
-      retry if try < 10
-      raise 'close_all failed'
+    if sizes[:buy] > 0
+      send_child_order(side: 'SELL', size: sizes[:buy])
+    elsif sizes[:sell] > 0
+      send_child_order(side: 'BUY', size: sizes[:sell])
     end
     puts "#{__method__}: EXIT"
   end
 
+private
   def position_sizes
-    puts "#{__method__}: ENTER"
+    # puts "#{__method__}: ENTER"
     positions = @private_client.positions(product_code: 'FX_BTC_JPY')
     buy  = 0.0
     sell = 0.0
@@ -92,12 +97,12 @@ class BitflyerGateway
     end
     buy  = buy.round(2) # ex) 0.045 + 0.005 = 0.049999999999999996
     sell = sell.round(2)
-    puts "#{__method__}: EXIT"
+    # puts "#{__method__}: EXIT"
     {buy: buy, sell: sell}
   end
 
   def position_price
-    puts "#{__method__}: ENTER"
+    # puts "#{__method__}: ENTER"
 
     try = 0
     begin
@@ -123,14 +128,13 @@ class BitflyerGateway
     end
     avg_price = (price / size).floor
     puts "#{__method__}: avg price=#{avg_price}"
-    puts "#{__method__}: EXIT"
+    # puts "#{__method__}: EXIT"
     avg_price
   end
 
   def order(side: nil, size: nil, profit: 1000.0, loss: 1000.0, risk: 0.02)
-    puts "#{__method__}: ENTER"
-    puts "#{__method__}: side = #{side}, size = #{size}, " +
-      "profit = #{profit}, loss = #{loss}, risk = #{risk}"
+    puts "#{__method__}: ENTER(side = #{side}, size = #{size}, " +
+      "profit = #{profit}, loss = #{loss}, risk = #{risk})"
 
     # ==========================================================================
     # 現在の値段を取得する
@@ -146,41 +150,28 @@ class BitflyerGateway
     # 預入証拠金の risk_parcentage までの損失に抑える量にする
     # --------------------------------------------------------------------------
     if size == nil
-      collateral = @private_client.collateral['collateral'] # 預入証拠金
-      amount_at_risk = (collateral * risk).floor # 許容損失額（リスク）
-      size = (amount_at_risk / loss).round(2)         # 売買するサイズ
-      if (size * current_price) * 1.01 > collateral * 4     # 購入できるか？
-        size = ((collateral * 4) / current_price).round(2)  # 購入できる取引量に
-        size = (size * 100 - 1) / 100                       # 誤差修正
-      end
+      size = size_at_risk(current_price: current_price, loss: loss, risk: risk)
     end
     # --------------------------------------------------------------------------
 
     # ==========================================================================
     # 注文を発注する
     # --------------------------------------------------------------------------
-    s = {product_code: 'FX_BTC_JPY',
-      child_order_type: 'MARKET', side: side, size: size}
-    puts "#{__method__}: CALL: send_child_order(#{s})"
-    r =  @private_client.send_child_order(s)
-    puts "#{__method__}: r = #{r}"
-    if r['status'] != nil
-      raise "#{s}\n#{r}" # 発注失敗
-    end
+    send_child_order(side: side, size: size)
     # --------------------------------------------------------------------------
 
     # ==========================================================================
-    # 平均取得単価を求める
+    # 建玉から平均取得単価を求める
     # --------------------------------------------------------------------------
     begin
-      current_price = position_price
+      current_price = average_price
     rescue
-      puts 'could not get position price. use mid price.'
+      puts 'could not get average price of current positions. use mid price.'
     end
-    puts "#{__method__}: #{current_price}"
+    # --------------------------------------------------------------------------
 
     # ==========================================================================
-    # 決済注文を発注する
+    # 利確・損切の価格を計算する
     # --------------------------------------------------------------------------
     case side
     when 'BUY'
@@ -192,7 +183,57 @@ class BitflyerGateway
       limit = current_price - profit
       stop  = current_price + loss
     end
+    # --------------------------------------------------------------------------
 
+    # ==========================================================================
+    # 決済注文を発注する
+    # --------------------------------------------------------------------------
+    send_parent_order(oposite_side: oposite_side,size: size,
+      limit: limit,stop: stop)
+    # --------------------------------------------------------------------------
+  end
+
+  def send_child_order(side:, size:)
+    # puts "#{__method__}: ENTER(side: = #{side},size: = #{size})"
+    with_retry() do
+      s = {product_code: 'FX_BTC_JPY', child_order_type: 'MARKET',
+        side: side, size: size}
+      puts "#{__method__}: CALL: send_child_order(#{s})"
+      r =  @private_client.send_child_order(s)
+      puts "#{__method__}: r = #{r}"
+      if r['status'] != nil
+        sleep(0.1)
+        raise "s=#{s}\nr=#{r}" # 発注失敗
+      end
+    end
+    # puts "#{__method__}: EXIT"
+  end
+
+  def cancel_all_child_orders
+    # puts "#{__method__}: ENTER"
+    s = {product_code: 'FX_BTC_JPY'}
+    puts "#{__method__}: CALL: cancel_all_child_orders(#{s})"
+    r = @private_client.cancel_all_child_orders(s)
+    puts "#{__method__}: r = #{r}"
+    # puts "#{__method__}: EXIT"
+  end
+
+  def size_at_risk(current_price:, loss:, risk:)
+    # puts "#{__method__}: ENTER"
+    collateral = @private_client.collateral['collateral'] # 預入証拠金
+    amount_at_risk = (collateral * risk).floor # 許容損失額（リスク）
+    size = (amount_at_risk / loss).round(2)         # 売買するサイズ
+    if (size * current_price) * 1.01 > collateral * 4     # 購入できるか？
+      size = ((collateral * 4) / current_price).round(2)  # 購入できる取引量に
+      size = (size * 100 - 1) / 100                       # 誤差修正
+    end
+    puts "#{__method__}: size = #{size}"
+    # puts "#{__method__}: EXIT"
+    size
+  end
+
+  def send_parent_order(oposite_side:,size:,limit:,stop:)
+    # puts "#{__method__}: ENTER"
     parameters = [{
       "product_code": "FX_BTC_JPY",
       "condition_type": "STOP",
@@ -208,13 +249,15 @@ class BitflyerGateway
       "size": size
     }]
 
-    s = {order_method: 'OCO', parameters: parameters}
-    puts "#{__method__}: CALL send_parent_order(#{s})"
-    r = @private_client.send_parent_order(s)
-    puts "#{__method__}: r = #{r}"
-    if r['status'] != nil
-      raise "#{s}\n#{r}" # 発注失敗
+    with_retry() do
+      s = {order_method: 'OCO', parameters: parameters}
+      puts "#{__method__}: CALL send_parent_order(#{s})"
+      r = @private_client.send_parent_order(s)
+      puts "#{__method__}: r = #{r}"
+      if r['status'] != nil
+        raise "#{s}\n#{r}" # 発注失敗
+      end
     end
-    puts "#{__method__}: EXIT"
+    # puts "#{__method__}: EXIT"
   end
 end
